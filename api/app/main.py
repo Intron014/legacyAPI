bd3c092aa7599bc3d1cfb53283f806ec2a1ce53d
Jorge Benjumea <me@intron014.com>
2024-07-17 13:27:36 +0200

    hmm

--- api/app/main.py
+++ api/app/main.py
@@ -1,18 +1,26 @@
 import logging
 from flask import Flask, redirect, jsonify, request
+from flask_migrate import Migrate
 from app.bicimad.routes import bicimad_bp
 from app.clipboard.routes import clipboard_bp
 from app.lastfm.routes import lastfm_bp
 from app.mood.routes import mood_bp
-from flask_sqlalchemy import SQLAlchemy
+from app.database import db
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ledb.db'
 
-from app.database import db
+
+
 db.init_app(app)
+migrate = Migrate(app, db)
+
+# with app.app_context():
+#     db.create_all()
+
+
 
 
 LOG_FORMAT = ("%(levelname) -10s %(asctime)s %(name) "
@@ -25,6 +33,7 @@ app.register_blueprint(clipboard_bp)
 app.register_blueprint(lastfm_bp)
 app.register_blueprint(mood_bp)
 
+
 @app.route('/<path:path>')
 def catch_all(path):
     return redirect("https://intron014.com/404api", code=302)
@@ -32,9 +41,12 @@ def catch_all(path):
 def root():
     return redirect("https://intron014.com/404api", code=302)
 
-
-if __name__ == '__main__':
-    # Create the database tables
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#         log.info(f"Database path: {app.config['SQLALCHEMY_DATABASE_URI']}")
+#     app.run(debug=True)
+# else:
+#     with app.app_context():
+#         db.create_all()
+#         log.info(f"Database path: {app.config['SQLALCHEMY_DATABASE_URI']}")
--- api/app/models.py
+++ api/app/models.py
@@ -2,10 +2,10 @@ from app.database import db
 from datetime import datetime
 
 class Moods(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     mood = db.Column(db.String(200), nullable=False)
 
 class MoodLogger(db.Model):
-    timestamp = db.Column(db.DateTime, primary_key=True, default=datetime.now())
-    mood_id = db.Column(db.Integer, db.ForeignKey('moods.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mood_id = db.Column(db.Integer, db.ForeignKey('moods.id'), nullable=False, autoincrement=True)
     mood = db.relationship('Moods', backref=db.backref('moodlog', lazy=True))
