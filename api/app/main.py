b0da5330091dd3024c0aa3ca07a0b72345159b4e
Jorge Benjumea <me@intron014.com>
2024-07-17 09:30:14 +0200

    database moved outta there

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, redirect
+from flask import Flask, redirect, jsonify, request
 from app.bicimad.routes import bicimad_bp
 from app.clipboard.routes import clipboard_bp
 from app.lastfm.routes import lastfm_bp
@@ -11,26 +11,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ledb.db'
 
-db = SQLAlchemy(app)
-
-class Moods(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mood = db.Column(db.String(200), nullable=False)
-
-class MoodLogger(db.Model):
-    timestamp = db.Column(db.DateTime, primary_key=True)
-    mood_id = db.Column(db.Integer, db.ForeignKey('moods.id'), nullable=False)
-    mood = db.relationship('Moods', backref=db.backref('moodlog', lazy=True))
-
-app.app_context().push()
-db.create_all()
+from app.database import db
+db.init_app(app)
 
 
 LOG_FORMAT = ("%(levelname) -10s %(asctime)s %(name) "
               "-30s %(funcName) -35s %(lineno) -5d: %(message)s")
 logging.basicConfig(level=os.environ.get(
     'LOG_LEVEL', 'INFO'), format=LOG_FORMAT)
-
+log = logging.getLogger(__name__)
 app.register_blueprint(bicimad_bp)
 app.register_blueprint(clipboard_bp)
 app.register_blueprint(lastfm_bp)
@@ -42,3 +31,10 @@ def catch_all(path):
 @app.route('/')
 def root():
     return redirect("https://intron014.com/404api", code=302)
+
+
+if __name__ == '__main__':
+    # Create the database tables
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ api/app/models.py
@@ -0,0 +1,11 @@
+from app.database import db
+from datetime import datetime
+
+class Moods(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mood = db.Column(db.String(200), nullable=False)
+
+class MoodLogger(db.Model):
+    timestamp = db.Column(db.DateTime, primary_key=True, default=datetime.now())
+    mood_id = db.Column(db.Integer, db.ForeignKey('moods.id'), nullable=False)
+    mood = db.relationship('Moods', backref=db.backref('moodlog', lazy=True))
